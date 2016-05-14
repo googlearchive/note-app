c3b45b3fc7db70e4c2f7eaf02fcb0ed7e2386a67
Chris Joel <cdata@google.com>
2016-05-14 16:25:24 -0700

    carbon -> app

--- common/sw-import.js
+++ common/sw-import.js
@@ -1,5 +1,5 @@
 /**
  * NOTE: This script is only here for the demo. It is not an important part of
- * the carbon-storage component.
+ * the app-storage component.
  */
 importScripts('bower_components/platinum-sw/service-worker.js');
